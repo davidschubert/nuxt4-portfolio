09a1a70379539808cb96304c72f0a07037557464
David Schubert <github@davidschubert.com>
2025-10-03 22:14:26 -1000

    pm2 update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,17 @@
+// ecosystem.config.cjs
 module.exports = {
     apps: [
         {
-            name: "NuxtAppName",
-            port: "3000",
-            exec_mode: "cluster",
-            instances: "max",
-            script: "./.output/server/index.mjs",
+            name: "{REPOSITORY_NAME}", // z.B. nuxt4-portfolio
+            script: ".output/server/index.mjs",
+            instances: 1,
+            exec_mode: "fork",
+            env: {
+                NODE_ENV: "production",
+                NITRO_HOST: "127.0.0.1",
+                NITRO_PORT: "3000",
+                PORT: "3000",
+            },
         },
     ],
 };
